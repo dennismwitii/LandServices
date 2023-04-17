7d78b1872952d80f089eced9a1f66330cb4fd98c
dennismwitii <dennisbmwiti@gmail.com>
2023-04-17 21:52:24 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+            <footer id="page-footer" class="opacity-0">
+                <div class="content py-20 font-size-xs clearfix">
+                    <div class="float-right">
+                         <i class="fa fa-heart text-pulse"></i>
+                    </div>
+                    <div class="float-left">
+                       <h5 style="color: black"> Spring Garden Web Application </h5>
+                    </div>
+                </div>
+            </footer>
+            <!-- END Footer -->
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+    
+    <!-- copyright -->
+    <div class="agileits-w3layouts">
+        <div class="container">
+            <p>© 2023 Spring Garden Web Application | </p>
+        </div>
+    </div>
+    <!-- //copyright -->
